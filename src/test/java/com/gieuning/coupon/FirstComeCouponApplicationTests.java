126f81fbecf7e32bbf5d7d96fc2fb1d9e97c559b
gieun <gieun.eom@astems.co.kr>
2026-09-03 17:01:48 +0900

    test: Testcontainers 기반 테스트 DB 환경 구성

--- src/test/java/com/gieuning/coupon/FirstComeCouponApplicationTests.java
+++ src/test/java/com/gieuning/coupon/FirstComeCouponApplicationTests.java
@@ -1,9 +1,12 @@
 package com.gieuning.coupon;
 
+import com.gieuning.coupon.global.config.MySqlTestContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(MySqlTestContainerConfig.class)
 class FirstComeCouponApplicationTests {
 
 	@Test
